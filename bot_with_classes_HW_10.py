522d2a45a11434fab42c2d5237c7457c7ed928c0
tetianakondra <tatjana.ukon@gmail.com>
2022-12-24 23:29:14 +0200

    address book is as UserDict, all contacts are saved to self.data

--- bot_with_classes_HW_10.py
+++ bot_with_classes_HW_10.py
@@ -8,15 +8,20 @@ class PhoneNotInt(Exception):
 class AddressBook(UserDict):
 
     user_id = 0
-    address_book = {}
 
+    
     def add_record(self, new_user):
-        for key, val in new_user.items():
 
-            self.data[key] = val
-            
         AddressBook.user_id += 1
-        AddressBook.address_book.update(self.data)
+        
+        if type(new_user) == str:
+            return new_user
+
+        for key, val in new_user.items():
+            self.data[key] = val
+            return {key: val}
+
+    def get_contacts(self):
         return self.data
 
 class Field:
@@ -33,13 +38,13 @@ class Record(Field):
         self.phone_numb = phones
         return self.phone_numb
 
-    def add_user(self, name, phones):
+    def add_user(self, name, phones, address_book):
 
         new_user = {}
 
-        if len(AddressBook().address_book):
+        if len(address_book):
 
-            for elem in AddressBook().address_book:
+            for elem in address_book:
                 if name in elem:
                     name = name + str(AddressBook.user_id)
 
@@ -47,16 +52,15 @@ class Record(Field):
 
         return new_user
 
-    def change_number(self, name, phones):
-        for key in AddressBook().address_book: 
+    def change_number(self, name, phones, address_book):
+        for key in address_book: 
             if key == name:
-                AddressBook().address_book[name] = phones
-                return AddressBook().address_book[name]
+                return {name: phones}
         return f"There is no user with name {name}"
 
-    def show_phone(self, name):
+    def show_phone(self, name, address_book):
         
-        for key, val in AddressBook().address_book.items():
+        for key, val in address_book.items():
             if key == name:
                 return f"the user {key} has the next phone numbers {val}"
         return f"There is not {name} in the Address book"
@@ -74,9 +78,6 @@ class Phone(Field):
 
         self.phones = phones
         
-
-        #while not self.phones:
-            #raise PhoneNotInt
 
         while True:
 
@@ -101,8 +102,11 @@ def main():
     It stops the process when the key words are entered
 
     """
-
+    
+    contacts = AddressBook()
     while True:
+
+        change_user = Record()
 
         user_command = input("Enter your command and data or enter 'help' to get the manual of bot  ")
         user_command_small_letters = user_command.lower()
@@ -116,26 +120,26 @@ def main():
             if user_command_small_letters.startswith(word):
 
                 user_data = user_command.split(" ")
+
                 
                 if user_data[0].lower() == "add":
                     if len(user_data) > 2:
-                        add_new_user = AddressBook()
-                        print(f"add to the address book {add_new_user.add_record(Record().add_user(Record().record_name(Name().user_name_def(user_data[1])), Record().record_phone(Phone().user_phones_def(user_data[2:]))))}")
+                        print(f"add to the address book {contacts.add_record(change_user.add_user(change_user.record_name(Name().user_name_def(user_data[1])), change_user.record_phone(Phone().user_phones_def(user_data[2:])), contacts.get_contacts()))}")
 
                 elif user_data[0].lower() == "help":
                     print("Enter 'add' and user's name and phone to add the user, 'change' and user's name and phone for changing the phone number, 'phone' and user's name to show the phone number, 'show all' to see the all users")
 
                 elif user_command_small_letters.startswith("show all"):
-                    print(AddressBook().address_book)
+                    print(contacts.get_contacts())
 
                 elif user_data[0].lower() == "change":
                     if len(user_data) > 2:
-                        change_user = Record()
-                        print(f"changes in the address book: for {change_user.record_name(Name().user_name_def(user_data[1]))} result {change_user.change_number(change_user.record_name(Name().user_name_def(user_data[1])), change_user.record_phone(Phone().user_phones_def(user_data[2:])))}")
+                        contacts.add_record(change_user.change_number(change_user.record_name(Name().user_name_def(user_data[1])), change_user.record_phone(Phone().user_phones_def(user_data[2:])), contacts.get_contacts()))
+                        print(f"changes in the address book: for {change_user.record_name(Name().user_name_def(user_data[1]))} result {change_user.change_number(change_user.record_name(Name().user_name_def(user_data[1])), change_user.record_phone(Phone().user_phones_def(user_data[2:])), contacts.get_contacts())}")
 
                 elif user_data[0].lower() == "phone":
                     if len(user_data) > 1:
-                        print(Record().show_phone(Name().user_name_def(user_data[1])))
+                        print(change_user.show_phone(Name().user_name_def(user_data[1]), contacts.get_contacts()))
                     
                     
 
